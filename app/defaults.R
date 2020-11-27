ebfccea6bed9b8117804d485bb2575570b6bb602
Ben J. Smith <benjsmith@gmail.com>
2020-11-27 14:56:09 +1300

    added a notebook for sim panel analysis. changed some defaults

--- app/defaults.R
+++ app/defaults.R
@@ -6,7 +6,7 @@ default_assumed_spread <- 0.02
 default_general_travel_rate <- 0.2
 default_current_lockdown_passenger_volume <- 11271 # august
 #https://www.customs.govt.nz/covid-19/more-information/passenger-statistics/
-default_traveler_relative_prevalence <- 0.69
+default_traveler_relative_prevalence <- 1.0#0.69
 #derived to bring our predicted "status quo" predicted cases/month numbers to match what we are actually observing at the border.
 
 
