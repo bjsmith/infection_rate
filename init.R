d96a4ffeb4eea3b0b1cbacecf843a67cc1d4f07d
Ben J. Smith <benjsmith@gmail.com>
2023-12-03 20:20:33 -0800

    get the app set up for running on heroku itself

---
+++ init.R
@@ -0,0 +1,39 @@
+
+
+app_packages <- c(
+  #shiny
+  "shiny",
+  "shinyjs",
+  "dplyr",
+  "shinybusy",
+  
+  #data
+  "DT",
+  "data.table",
+  "dplyr",
+  "lubridate",
+  "readr",
+  "stringr",
+  "magrittr",
+  "googlesheets4",
+  "zoo",
+  
+  #visual
+  "ggplot2", # tidyverse vis package
+  "ggrepel",
+  "sf",
+  "spData",
+  "leaflet",
+  "rnaturalearth",
+  "rnaturalearthdata",
+  "RColorBrewer"
+)
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
+
